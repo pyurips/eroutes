ed234be701046001cabd382e18f38d866854b74f
Pablo Yuri <pabloyuridev@gmail.com>
2024-02-19 18:21:29 -0300

    feat: add all http verbs

--- src/types/route-structure.ts
+++ src/types/route-structure.ts
@@ -3,7 +3,7 @@ import { middlewareStructure } from "./middleware-structure";
 
 export type routeStructure = {
   route: string;
-  method: "get" | "post" | "patch" | "put" | "delete";
+  method: "get" | "post" | "patch" | "put" | "delete" | "options" | "head" | "trace" | "connect";
   middleware: middlewareStructure | null;
   controller: controllerStructure;
 };
